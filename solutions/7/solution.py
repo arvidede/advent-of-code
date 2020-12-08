ee6c14eedeb45fbb8b1581ad4328b2d804f1e3dd
Arvid Edenheim <arvidede@icloud.com>
2020-12-08 22:57:25 +0100

    Day 7. Problem 2. My recursion has recursion has recursion has headache

--- solutions/7/solution.py
+++ solutions/7/solution.py
@@ -2,13 +2,18 @@ from sys import argv
 from functools import reduce
 
 
-def parse_bag(line):
+def parse_bag(line, with_count=False):
     bag, rest = line.split('contain')
     bag = bag[:-6]
     content = []
     if rest.strip() != 'no other bags.':
-        content = [' '.join(bag.strip().split(' ')[1:3])
-                   for bag in rest.split(',')]
+        if with_count:
+            content = [' '.join(bag.strip().split(' ')[0:3])
+                       for bag in rest.split(',')]
+            content = flatten([int(bag[0:1])*[bag[2:]] for bag in content])
+        else:
+            content = [' '.join(bag.strip().split(' ')[1:3])
+                       for bag in rest.split(',')]
     return bag, content
 
 
@@ -37,8 +42,16 @@ def __1__(lines, key='shiny gold'):
     return len(set(filter(lambda x: x != key, flatten(backtrack(graph, key)))))
 
 
-def __2__(lines):
-    return 2
+def recursion(graph, key):
+    return [[recursion(graph, bag), key] if len(graph[key]) > 0 else [bag, key] for bag in graph[key]]
+
+
+def __2__(lines, key='shiny gold'):
+    graph = {}
+    for line in lines:
+        bag, content = parse_bag(line, with_count=True)
+        graph[bag] = content
+    return len(flatten(recursion(graph, key)))
 
 
 def main():
