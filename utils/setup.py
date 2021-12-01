f51b1a24e02ab9909d277f22cbf6dbd8ebc082f9
Arvid Edenheim <arvidede@icloud.com>
2021-12-01 18:40:10 +0100

    Setup 2021

--- utils/setup.py
+++ utils/setup.py
@@ -6,8 +6,8 @@ import datetime
 import os
 
 
-def get_input(day):
-    url = f'https://adventofcode.com/2020/day/{day}/input'
+def get_input(year, day):
+    url = f'https://adventofcode.com/{year}/day/{day}/input'
     response = requests.get(
         url, cookies={'session': os.getenv('SESSION_COOKIE')})
     if response.status_code == 200:
@@ -17,11 +17,13 @@ def get_input(day):
 def setup():
     load_dotenv()
     root_dir = Path(__file__).absolute().parent
-    today = str(datetime.datetime.today().day)
-    data = get_input(today)
+    today = datetime.datetime.today()
+    day = str(today.day)
+    year = str(today.year)
+    data = get_input(year, day)
 
     if data:
-        path = root_dir.joinpath(f"../solutions/{today}").__str__()
+        path = root_dir.joinpath(f"../solutions/{year}/{day}").__str__()
         copy_tree(root_dir.joinpath('template').__str__(),
                   path)
         with open(path + '/data.txt', 'w+') as f:
