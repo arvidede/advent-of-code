1f6662f1a8af7be6d2b56e31917e26b3fa8fd9bd
Arvid Edenheim <arvidede@icloud.com>
2022-12-05 16:55:10 +0100

    Setup 2022

--- utils/setup.py
+++ utils/setup.py
@@ -1,15 +1,24 @@
 from distutils.dir_util import copy_tree
 from dotenv import load_dotenv
 from pathlib import Path
+from sys import argv
 import requests
 import datetime
 import os
 
 
+def get_year_day():
+    if len(argv) > 1:
+        return int(argv[1]), int(argv[2])
+    today = datetime.datetime.today()
+    return str(today.year), str(today.day)
+
+
 def get_input(year, day):
-    url = f'https://adventofcode.com/{year}/day/{day}/input'
+    url = f"https://adventofcode.com/{year}/day/{day}/input"
     response = requests.get(
-        url, cookies={'session': os.getenv('SESSION_COOKIE')})
+        url, cookies={"session": os.getenv("SESSION_COOKIE")}
+    )
     if response.status_code == 200:
         return response.text
 
@@ -17,16 +26,13 @@ def get_input(year, day):
 def setup():
     load_dotenv()
     root_dir = Path(__file__).absolute().parent
-    today = datetime.datetime.today()
-    day = str(today.day)
-    year = str(today.year)
+    year, day = get_year_day()
     data = get_input(year, day)
 
     if data:
         path = root_dir.joinpath(f"../solutions/{year}/{day}").__str__()
-        copy_tree(root_dir.joinpath('template').__str__(),
-                  path)
-        with open(path + '/data.txt', 'w+') as f:
+        copy_tree(root_dir.joinpath("template").__str__(), path)
+        with open(path + "/data.txt", "w+") as f:
             f.write(data)
 
 
--- utils/template/solution.py
+++ utils/template/solution.py
@@ -9,10 +9,14 @@ def __2__(lines):
     return 2
 
 
-def main():
-    file = argv[2] if len(argv) > 2 else 'data.txt'
+def parse_input():
+    file = argv[2] if len(argv) > 2 else "data.txt"
     lines = open(file).read().splitlines()
-    print({"1": __1__, "2": __2__}[argv[1]](lines))
+    return lines
+
+
+def main():
+    print({"1": __1__, "2": __2__}[argv[1]](parse_input()))
 
 
 main()
